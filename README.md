a5ba2b8bf78d56fe0fcb847eca4ea31b41b619dd
Ryan Clark <rclark@routeone.com>
2022-06-28 20:36:45 -0400

    Holy Bunions Batman! This commit has no parents...

---
+++ README.md
@@ -0,0 +1,1 @@
+# 😜 fantastic-sniffle
---
+++ js/main.js
@@ -0,0 +1,182 @@
+!(function($) {
+	"use strict";
+
+	$(window).stellar({
+		responsive: true,
+		parallaxBackgrounds: true,
+		parallaxElements: true,
+		horizontalScrolling: false,
+		hideDistantElements: false,
+		scrollProperty: "scroll"
+	});
+
+	let fullHeight = function() {
+		$(".js-fullheight").css("height", $(window).height());
+		$(window).resize(function() {
+			$(".js-fullheight").css("height", $(window).height());
+		});
+	};
+
+	fullHeight();
+
+	// loader
+	let loader = function() {
+		setTimeout(function() {
+			let spinner = $("#ftco-loader");
+			if (spinner.length > 0) {
+				spinner.removeClass("show");
+			}
+		}, 1);
+	};
+
+	loader();
+
+	let carousel = function() {
+		$(".carousel-testimony").owlCarousel({
+			center: true,
+			loop: true,
+			items: 1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				1000: {
+					items: 3
+				}
+			}
+		});
+
+	};
+
+	carousel();
+
+	$("nav .dropdown").hover(function() {
+		let $this = $(this);
+		// 	 timer;
+		// clearTimeout(timer);
+		$this.addClass("show");
+		$this.find("> a").attr("aria-expanded", true);
+		// $this.find(".dropdown-menu").addClass("animated-fast fadeInUp show");
+		$this.find(".dropdown-menu").addClass("show");
+	}, function() {
+		let $this = $(this);
+		// timer;
+		// timer = setTimeout(function(){
+		$this.removeClass("show");
+		$this.find("> a").attr("aria-expanded", false);
+		// $this.find(".dropdown-menu").removeClass("animated-fast fadeInUp show");
+		$this.find(".dropdown-menu").removeClass("show");
+		// }, 100);
+	});
+
+	$("#dropdown04").on("show.bs.dropdown", function() {
+		console.log("show");
+	});
+
+	// magnific popup
+	$(".image-popup").magnificPopup({
+		type: "image",
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don"t foget to change the duration also in CSS
+		}
+	});
+
+	$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+		disableOn: 700,
+		type: "iframe",
+		mainClass: "mfp-fade",
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+
+
+	let counter = function() {
+		$("#section-counter").waypoint(function(direction) {
+			if (direction === "down" && !$(this.element).hasClass("ftco-animated")) {
+				let comma_separator_number_step = $.animateNumber.numberStepFactories.separator(",")
+				$(".number").each(function() {
+					let $this = $(this),
+						num = $this.data("number");
+					console.log(num);
+
+					$this.animateNumber(
+						{
+							number: num,
+							numberStep: comma_separator_number_step
+						}, 7000
+					);
+				});
+
+			}
+
+		}, {offset: "95%"});
+
+	}
+
+	counter();
+
+	let contentWayPoint = function() {
+		let i = 0;
+		$(".ftco-animate").waypoint(function(direction) {
+			if (direction === "down" && !$(this.element).hasClass("ftco-animated")) {
+
+				i++;
+
+				$(this.element).addClass("item-animate");
+				setTimeout(function() {
+
+					$("body .ftco-animate.item-animate").each(function(k) {
+						let el = $(this);
+						setTimeout(function() {
+							let effect = el.data("animate-effect");
+							if (effect === "fadeIn") {
+								el.addClass("fadeIn ftco-animated");
+							} else if (effect === "fadeInLeft") {
+								el.addClass("fadeInLeft ftco-animated");
+							} else if (effect === "fadeInRight") {
+								el.addClass("fadeInRight ftco-animated");
+							} else {
+								el.addClass("fadeInUp ftco-animated");
+							}
+							el.removeClass("item-animate");
+						}, k * 50, "easeInOutExpo");
+					});
+
+				}, 100);
+
+			}
+
+		}, {offset: "95%"});
+	};
+
+	contentWayPoint();
+
+	$(".appointment_date").datepicker({
+		"format": "m/d/yyyy",
+		"autoclose": true
+	});
+
+	$(".appointment_time").timepicker();
+	$("#copyright-year").text(new Date().getFullYear());
+})(jQuery);
